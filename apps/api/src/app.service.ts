14f4fc07731a489440dbea80deb507b17e9b6154
Nazira Fakhrurradi <nazira.fakhrurradi7@gmail.com>
2026-04-19 23:53:26 -0700

    omitting CD actions

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! - This will be changed';
+    return 'Hello World!';
   }
 }
